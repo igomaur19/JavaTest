2ea83ef8541f4c9f051cbcc53667d86c9d07d6e6
igomaur19 <igomaur19@gmail.com>
2023-03-22 18:01:16 +0700

    seccond commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello By The Way");
+		for(int i=2;i<=100;i++){
+			int counter = 0;
+			for(int j=i-1; j>1; j--){
+				if(j<2) continue;
+				if(i%j==0) counter++;
+			}
+			if(counter==0) System.out.println(i);
+		}
 	}
 }
